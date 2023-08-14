bac3ab1674b11436f7966ddb363d398c1bff29bd
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-08-14 21:21:06 +0000

    Deployed 2d0c0e6 with MkDocs version: 1.1.2

--- docs.sh
+++ docs.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/bash
 
-function github_only()
-{
-  git add .gitignore
-  git commit -m ".gitignore"
-  git add NOTES.md
-  git commit -m "NOTES"
-  git add README.md
-  git commit -m "README"
-  git add st.sh
-  git commit -m "BATCH script"
-  git push
-}
-
 function github_pages()
 {
   module load python/3.7
@@ -32,6 +19,4 @@ git add .gitignore
 git add docs
 git add mkdocs.yml
 git commit -m "backup"
-git add README.md
-git commit -m "README"
 git push
