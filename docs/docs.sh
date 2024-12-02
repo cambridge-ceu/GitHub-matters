6b9c81ccc93ceb3b2dc3d592b5c017555dd3fa85
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-12-02 10:08:37 +0000

    source

--- docs/docs.sh
+++ docs/docs.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/bash
 
-function github_pages()
+#!/usr/bin/bash
+
+function setup()
 {
   module load python/3.8
   source ~/rds/public_databases/software/py38/bin/activate
-# pip install pymdown-extensions
-}
-
-function install()
-{
   pip install mkdocs-mermaid2-plugin
+# pip install pymdown-extensions
 }
 
 function my_api()
@@ -20,13 +18,18 @@ function my_api()
 
 module load ceuadmin/libssh/0.10.6-icelake
 module load ceuadmin/openssh/9.7p1-icelake
+module load gettext/0.21/gcc/qnrcglqo
+module load ceuadmin/krb5
+module load libiconv/1.16/gcc/4miyzf3w
 
-github_pages
+setup
 mkdocs build
 mkdocs gh-deploy
 
 git add .gitignore
+git commit -m ".gitignore"
 git add docs my_api
+git commit -m "source"
 git add mkdocs.yml
-git commit -m "backup"
+git commit -m "mkdocs.yml"
 git push
